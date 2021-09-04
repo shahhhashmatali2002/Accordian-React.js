6932c12b7b489987fb733673001676e05031f8fb
Hashmat Ali Shah <89321559+shahhhashmatali2002@users.noreply.github.com>
2021-09-04 21:11:05 +0500

    Index.js File Added

---
+++ Accordian/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Accordian from './Accordian/Accordian'
+import Parent from './ContextAPI/Parent'
+
+ReactDOM.render(
+  <><Accordian/></>,
+  document.getElementById('root')
+);
